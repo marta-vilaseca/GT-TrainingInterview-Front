22b5e23fa0a70262a41898773e17e01167d7fcd0
Marta <vilaseca.marta@gmail.com>
2024-10-03 22:56:43 +0200

    Fixed a couple of minor errors related to SASS and TypeScript

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare module 'babel__core';
+declare module 'babel__generator';
+declare module 'babel__template';
+declare module 'babel__traverse';
+declare module 'estree';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createServer } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Any global SCSS settings can be added here
+      },
+    },
+  },
 });
