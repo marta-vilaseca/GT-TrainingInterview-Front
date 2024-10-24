3763e4d626642f3c40ad69e841b679b416e8152c
reyesjerez <reyesjerezpe@gmail.com>
2024-10-24 16:22:11 -0600

    [ADD]:pages

---
+++ src/pages/Chat.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/layout/Layout';
+
+function Chat() {
+  return (
+    <>
+      <Layout page="home" extraClassName="chat">
+        <h2>Chat</h2>
+      </Layout>
+    </>
+  );
+}
+
+export default Chat
