9e9f9b2529539e0e50bf746c40cbca8b708a2784
reyesjerez <reyesjerezpe@gmail.com>
2024-10-24 19:51:21 -0600

    [ADD]: Basic elements and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import Layout from '../components/layout/Layout';
+import Dora from '../assets/artificial-bot-intelligence-svgrepo-com.svg';
+import HomeForm from '../components/forms/HomeForm';
+import './Home.scss';
 
 function Home() {
   return (
     <>
       <Layout page="home" extraClassName="chat">
-        <h2>Home</h2>
+        <div className="greetings">
+          <img src={Dora} className="greetings__logo" alt="PildorasUX logo" />
+          <h2 className="greetings__title">
+            ¡Hola! Soy Dora, tu entrenadora virtual para entrevistas.
+          </h2>
+          <p className="greetings__body">
+            Estoy aquí para ayudarte a prepararte y mejorar tus respuestas de
+            forma personalizada.
+          </p>
+          <p className="greetings__body">
+            ¿Listo para comenzar tu entrenamiento?
+          </p>
+        </div>
+        <HomeForm></HomeForm>
       </Layout>
     </>
   );
