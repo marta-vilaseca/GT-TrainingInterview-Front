6b8670084b2ebbe00b2688d8b9126b95e3eaa1f3
reyesjerez <reyesjerezpe@gmail.com>
2024-10-28 14:56:08 -0600

    [ADD}: Login page

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,15 +1,12 @@
 import Layout from '../components/layout/Layout';
 import Dora from '../assets/artificial-bot-intelligence-svgrepo-com.svg';
-import Button from '../components/common/Button';
-import './Home.scss';
+import HomeForm from '../components/forms/HomeForm';
+import './LogIn.scss';
 
 function Home() {
-  const handleStart = () => {
-    console.log('Hello');
-  };
   return (
     <>
-      <Layout page="home" extraClassName="home">
+      <Layout page="login" extraClassName="login">
         <div className="greetings">
           <img src={Dora} className="greetings__logo" alt="PildorasUX logo" />
           <h2 className="greetings__title">
@@ -22,8 +19,8 @@ function Home() {
           <p className="greetings__body">
             ¿Listo para comenzar tu entrenamiento?
           </p>
-          <Button onClick={handleStart}>¡Empecemos!</Button>
         </div>
+        <HomeForm></HomeForm>
       </Layout>
     </>
   );
