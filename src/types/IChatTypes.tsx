4392472cacbf908125a93d48dd7c0f72005d5b43
Marta <vilaseca.marta@gmail.com>
2024-11-23 18:36:33 +0100

    [FIX]: Cleaned unused files to fic build errors

--- src/types/IChatTypes.tsx
+++ src/types/IChatTypes.tsx
@@ -1,20 +1,4 @@
 import { QuestionData2 } from './IAxios';
-// export interface ChatEntry {
-//   question: string;
-//   answer: string;
-//   feedback: string;
-// }
-
-// export interface ChatboxProps {
-//   onSubmit: (answer: string) => void;
-//   disabled: boolean;
-// }
-
-// export interface ControlsBoxProps {
-//   onSubmit: (answer: string) => void;
-//   isChatStarted: boolean;
-//   disabled: boolean;
-// }
 
 export interface ReviewQuestion {
   question: QuestionData2;
